c16a76abbe90a5dbba93c9dd79b62a9400ed38b9
Willy Gomez <wp_gomez@hotmail.com>
2018-08-11 13:33:53 -0500

    Inicio

---
+++ resources/lang/vendor/voyager/es/profile.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'avatar'            => 'Avatar',
+    'edit'              => 'Editar mi perfil',
+    'edit_user'         => 'Editar usuario',
+    'password'          => 'Contraseña',
+    'password_hint'     => 'Dejar vacío para mantener el mismo',
+    'role'              => 'Rol',
+    'user_role'         => 'Rol del usuario',
+    'role_default'      => 'Rol',
+    'roles_additional'  => 'Roles',
+];
